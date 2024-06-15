076c95cde12627ba36ebae52f0fac8125787bf96
TimLiz <89855787672@mail.ru>
2024-06-16 01:21:39 +0300

    1

--- init.lua
+++ init.lua
@@ -269,7 +269,7 @@ if readFile and writeFile then
                 local content
 
                 if not ran then
-                    content = game:HttpGetAsync("https://raw.githubusercontent.com/" .. user .. "/Hydroxide/" .. branch .. '/' .. asset .. ".lua?token=$(date%20+%s")
+                    content = game:HttpGetAsync("https://raw.githubusercontent.com/" .. user .. "/Hydroxide/" .. branch .. '/' .. asset .. ".lua?token=1")
                     writeFile(file, content)
                 else
                     content = result
